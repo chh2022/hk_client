6e01ad5b9307f2438bf73782adb8b8a6975cdced
chh2022 <b9102214@gmail.com>
2022-09-01 22:31:13 +0800

    a. A button to start sending request b. A textbox records the server ip c. A textbox records the server port d. A textbox records the file name e. A textbox records file path to save in the client computer f. A textbox shows the successful or error messages when sending requests

---
+++ hk_client/client/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "send request";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PORT:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "FILE NAME:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "SAVE PATH:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(89, 69);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(134, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(90, 97);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(134, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Message:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(89, 120);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(134, 22);
+            this.textBox5.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 186);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ hk_client/client/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using INIRW;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+        string IP;
+        string Port;
+        string FileName;
+        string FilePath;
+        TcpClient tcpClient;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void SendMsg(string msg, TcpClient tmpTcpClient)
+        {
+            NetworkStream ns = tmpTcpClient.GetStream();
+            if (ns.CanWrite)
+            {
+                byte[] msgByte = Encoding.Default.GetBytes(msg);
+                ns.Write(msgByte, 0, msgByte.Length);
+            }
+        }
+        public string ReceiveMsg(TcpClient tmpTcpClient)
+        {
+            string receiveMsg = string.Empty;
+            byte[] receiveBytes = new byte[tmpTcpClient.ReceiveBufferSize];
+            int numberOfBytesRead = 0;
+            NetworkStream ns = tmpTcpClient.GetStream();
+
+            if (ns.CanRead)
+            {
+                do
+                {
+                    numberOfBytesRead = ns.Read(receiveBytes, 0, tmpTcpClient.ReceiveBufferSize);
+                    receiveMsg = Encoding.Default.GetString(receiveBytes, 0, numberOfBytesRead);
+                }
+                while (ns.DataAvailable);
+            }
+            return receiveMsg;
+        }
+        public void Receivefile(TcpClient tmpTcpClient)
+        {
+            string receiveMsg = string.Empty;
+            byte[] receiveBytes = new byte[tmpTcpClient.ReceiveBufferSize];
+            int numberOfBytesRead = 0;
+            NetworkStream ns = tmpTcpClient.GetStream();
+
+            if (ns.CanRead)
+            {
+                do
+                {
+                    numberOfBytesRead = ns.Read(receiveBytes, 0, tmpTcpClient.ReceiveBufferSize);
+                    
+                }
+                while (ns.DataAvailable);
+            }
+            /*SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "*.*|*.*";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                fs.Write(receiveBytes, 0, receiveBytes.Length);
+                fs.Close();
+            }*/
+            string strSavePath = FilePath + FileName; 
+            FileStream fs = new FileStream(strSavePath, FileMode.Create);
+            fs.Write(receiveBytes, 0, receiveBytes.Length);
+            fs.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //button1.Enabled = false;
+                if (tcpClient==null)
+                {
+                    //預設主機IP
+                    string hostIP = IP;
+
+                    //先建立IPAddress物件,IP為欲連線主機之IP
+                    IPAddress ipa = IPAddress.Parse(hostIP);
+
+                    //建立IPEndPoint
+                    IPEndPoint ipe = new IPEndPoint(ipa, Int32.Parse(Port));
+
+                    //先建立一個TcpClient;
+                    //TcpClient tcpClient = new TcpClient();
+                    tcpClient = new TcpClient();
+                    tcpClient.Connect(ipe);
+                }
+                
+
+                if (tcpClient.Connected)
+                {
+                    string strFile = FileName;
+
+                    SendMsg(strFile, tcpClient);
+                    string str = ReceiveMsg(tcpClient);
+                    if (str == "@file found")
+                    {
+                        Receivefile(tcpClient);
+                        textBox5.Text = "successful";
+                    }
+                    else
+                    {
+                        textBox5.Text = "error";
+                    }
+                    //textBox1.Text = str;
+                    //CommunicationBase cb = new CommunicationBase();
+                    //cb.SendMsg("這是客戶端傳給主機的訊息", tcpClient);
+
+                }
+            }
+            catch(Exception ex)
+            {
+                button1.Enabled = true;
+                MessageBox.Show(ex.Message);
+            }
+           
+
+        }
+
+
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            var MyIni = new IniFile("Settings.ini");
+            IP = MyIni.Read("IP");
+            Port = MyIni.Read("PORT");
+            FileName = MyIni.Read("FILE_NAME");
+            FilePath = MyIni.Read("FILE_PATH");
+            textBox1.Text = IP;
+            textBox2.Text = Port;
+            textBox3.Text = FileName;
+            textBox4.Text = FilePath;
+        }
+    }
+}
